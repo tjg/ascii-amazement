898d7735dcf9abf2326d657015b86a55b1ae160f
Tj Gabbour <tjg@pentaside.org>
2013-12-07 14:51:02 +0100

    Fix whitespace.

--- test/weft/core_test.clj
+++ test/weft/core_test.clj
@@ -4,11 +4,11 @@
             [weft.maze :as maze]))
 
 (facts "Solves all the test mazes which ITA provided"
- (doseq [n (range 1 7)]
-   (let [expected-input-file  (format "resources/mazes/input%s.txt"  n)
-         expected-output-file (format "resources/mazes/output%s.txt" n)
-         expected-output (slurp expected-output-file)]
-     (fact "Solves maze exactly as ITA did"
+  (doseq [n (range 1 7)]
+    (let [expected-input-file  (format "resources/mazes/input%s.txt"  n)
+          expected-output-file (format "resources/mazes/output%s.txt" n)
+          expected-output (slurp expected-output-file)]
+      (fact "Solves maze exactly as ITA did"
         (core/write-solution expected-input-file "out.txt") => irrelevant
         (provided
           (spit "out.txt" expected-output) => irrelevant :times 1)))))
