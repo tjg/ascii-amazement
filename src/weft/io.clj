b97f5de9766ab9830550b91a1b1d1f7e9354f84b
Tj Gabbour <tjg@pentaside.org>
2013-12-07 09:52:31 +0100

    Maze input.

---
+++ src/weft/io.clj
@@ -0,0 +1,47 @@
+(ns weft.io
+  (:require [clojure.pprint :as pprint :refer [pprint cl-format]]
+            [clojure.java.io :as io]))
+
+(set! *print-length* 10)
+
+(defn read-maze [path]
+  (with-open [rdr (io/reader "resources/mazes/input1.txt")]
+    (->> (line-seq rdr)
+         doall
+         (into (vector)))))
+
+(defn get-coordinates [marker coordinate-translation lines]
+  (let [[line-nr idx] (->> (map (fn [line-nr]
+                                  [line-nr (.indexOf (nth lines line-nr)
+                                                     marker)])
+                                (range (count lines)))
+                           (filter (fn [[line-nr idx]]
+                                     (not= idx -1)))
+                           first)
+        [line-nr idx] (coordinate-translation [line-nr idx])]
+    [line-nr idx]))
+
+(defn start-coordinates [lines]
+  (let [start-marker "Start|"]
+    (get-coordinates start-marker
+                     (fn [[line-nr idx]]
+                       [line-nr (+ idx (count start-marker))])
+                     lines)))
+
+(defn end-coordinates [lines]
+  (let [start-marker "ITA"]
+    (get-coordinates start-marker
+                     (fn [[line-nr idx]]
+                       [(inc line-nr) (dec idx)])
+                     lines)))
+
+
+(comment
+  (doseq [line (read-maze nil)]
+    (println line))
+
+  (->> (read-maze nil)
+       start-coordinates)
+
+  (->> (read-maze nil)
+       end-coordinates))
