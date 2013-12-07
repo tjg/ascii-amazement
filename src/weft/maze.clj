da34ff63eb65722148414a598b95ddd6dd364a56
Tj Gabbour <tjg@pentaside.org>
2013-12-07 12:05:41 +0100

    Renaming files

--- src/weft/maze.clj
+++ src/weft/maze.clj
@@ -1,20 +1,130 @@
 (ns weft.maze
   (:require [clojure.pprint :as pprint :refer [pprint cl-format]]
-            [weft.search :as search]
-            [weft.io :as io]))
+            [clojure.java.io :as io]))
 
-(defn solve [maze]
-  (search/depth-first-search (io/start-coordinates maze)
-                             (fn [pos]
-                               (let [[line-offset idx-offset]
-                                     (io/coordinate- pos (io/end-coordinates maze))]
-                                 (and (>= 1 (Math/abs line-offset))
-                                      (>= 1 (Math/abs idx-offset)))))
-                             (fn [pos]
-                               (io/children maze pos))))
+(set! *print-length* 10)
+
+(defn read-maze [path]
+  (with-open [rdr (io/reader path)]
+    (->> (line-seq rdr)
+         doall
+         (into (vector)))))
+
+(defn get-coordinates [lines marker coordinate-translation]
+  (let [[line-nr idx] (->> (map (fn [line-nr]
+                                  [line-nr (.indexOf (nth lines line-nr)
+                                                     marker)])
+                                (range (count lines)))
+                           (filter (fn [[line-nr idx]]
+                                     (not= idx -1)))
+                           first)
+        [line-nr idx] (coordinate-translation [line-nr idx])]
+    [line-nr idx]))
+
+(defn start-coordinates [lines]
+  (let [start-marker "Start|"]
+    (get-coordinates lines
+                     start-marker
+                     (fn [[line-nr idx]]
+                       [line-nr (+ idx (count start-marker))]))))
+
+(defn end-coordinates [lines]
+  (let [start-marker "ITA"]
+    (get-coordinates lines
+                     start-marker
+                     (fn [[line-nr idx]]
+                       [(inc line-nr) (dec idx)]))))
+
+(defn coordinate- [[y0 x0] [y1 x1]]
+  [(- y0 y1) (- x0 x1)])
+
+(defn coordinate+ [[y0 x0] [y1 x1]]
+  [(+ y0 y1) (+ x0 x1)])
+
+(defn char-at [maze [line-nr idx]]
+  (nth (nth maze line-nr)
+       idx))
+
+(defn down [maze pos]
+  (try
+    (let [step-0 (char-at maze pos)
+          step-1 (char-at maze (coordinate+ pos [1 0]))
+          step-2 (char-at maze (coordinate+ pos [2 0]))
+          new-pos (coordinate+ pos [2 0])]
+      (if (and (= step-0 \space)
+               (= step-1 \space)
+               (or (= step-2 \space)
+                   (= step-2 \_)))
+        new-pos
+        false))
+    (catch Exception e
+      false)))
+
+(defn up [maze pos]
+  (try
+    (let [step-0 (char-at maze pos)
+          step-1 (char-at maze (coordinate+ pos [-1 0]))
+          step-2 (char-at maze (coordinate+ pos [-2 0]))
+          new-pos (coordinate+ pos [-2 0])]
+      (if (and (= step-1 \space)
+               (= step-2 \space))
+        new-pos
+        false))
+    (catch Exception e
+      false)))
+
+(defn left [maze pos]
+  (try
+    (let [step-0 (char-at maze pos)
+          step-1 (char-at maze (coordinate+ pos [0 -1]))
+          step-2 (char-at maze (coordinate+ pos [0 -2]))
+          new-pos (coordinate+ pos [0 -2])]
+      (if (and (or (= step-1 \space)
+                   (= step-1 \_))
+               (or (= step-2 \space)
+                   (= step-2 \_)))
+        new-pos
+        false))
+    (catch Exception e
+      false)))
+
+(defn right [maze pos]
+  (try
+    (let [step-0 (char-at maze pos)
+          step-1 (char-at maze (coordinate+ pos [0 1]))
+          step-2 (char-at maze (coordinate+ pos [0 2]))
+          new-pos (coordinate+ pos [0 2])]
+      (if (and (or (= step-1 \space)
+                   (= step-1 \_))
+               (or (= step-2 \space)
+                   (= step-2 \_)))
+        new-pos
+        false))
+    (catch Exception e
+      false)))
+
+(defn children [maze pos]
+  (let [moves (->> [up down left right]
+                   (map #(% maze pos))
+                   (filter identity))]
+    moves))
+
+
+(def test-maze-path "resources/mazes/input1.txt")
+
+(def test-maze (read-maze test-maze-path))
 
 (comment
-  (let [solution (solve io/test-maze)]
-    (println (count (search/path solution)))
-    (doseq [pos (search/path solution)]
-      (println pos))))
+  (doseq [line (read-maze test-maze-path)]
+    (println line))
+
+  (->> (read-maze test-maze-path)
+       start-coordinates)
+
+  (->> (read-maze test-maze-path)
+       end-coordinates)
+
+  (children test-maze (start-coordinates test-maze))
+
+  (wall? test-maze [1 34])
+  (wall? test-maze [0 34]))
---
+++ src/weft/solve.clj
@@ -0,0 +1,21 @@
+(ns weft.solve
+  (:require [clojure.pprint :as pprint :refer [pprint cl-format]]
+            [weft.search :as search]
+            [weft.maze :as maze]))
+
+(defn solve [maze]
+  (search/depth-first-search (maze/start-coordinates maze)
+                             (fn [pos]
+                               (let [[line-offset idx-offset]
+                                     (maze/coordinate- pos
+                                                       (maze/end-coordinates maze))]
+                                 (and (>= 1 (Math/abs line-offset))
+                                      (>= 1 (Math/abs idx-offset)))))
+                             (fn [pos]
+                               (maze/children maze pos))))
+
+(comment
+  (let [solution (solve maze/test-maze)]
+    (println (count (search/path solution)))
+    (doseq [pos (search/path solution)]
+      (println pos))))
