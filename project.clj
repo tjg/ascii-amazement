82ab0a2923608d91d0cb9b25762822aaa6bb6045
Tj Gabbour <tjg@pentaside.org>
2013-12-07 18:30:06 +0100

    Remove unused dependency.

--- project.clj
+++ project.clj
@@ -5,6 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ascii-amazement.core
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.2.4"]
-                 [com.taoensso/timbre "2.7.1"]]
+                 [org.clojure/tools.cli "0.2.4"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
