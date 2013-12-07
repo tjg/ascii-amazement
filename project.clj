4ff2227c85de2ed64f7f9cd8dce3beafca49ad04
Tj Gabbour <tjg@pentaside.org>
2013-12-07 14:23:09 +0100

    Run on commandline.

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main weft.core
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [com.taoensso/timbre "2.7.1"]])
--- src/weft/core.clj
+++ src/weft/core.clj
@@ -1,2 +1,24 @@
 (ns weft.core
+  (:gen-class)
   (:require [clojure.pprint :as pprint :refer [pprint cl-format]]
+            [weft.solve :as solve]
+            [weft.maze :as maze]
+            [clojure.tools.cli :as cli :refer [cli]]))
+
+(defn write-solution [in-path out-path]
+  (let [maze (maze/read-maze in-path)
+        solution (solve/solve maze)]
+    (maze/write-maze out-path maze solution)))
+
+(defn -main [& args]
+  (let [[options args banner]
+        (cli args
+             ["-h" "--help" "Show help" :default false]
+             ["-i" "--input" "Input maze path" :default false]
+             ["-o" "--output" "Output maze path" :default false])]
+    (if (or (:help options)
+            (not (:input options))
+            (not (:output options)))
+      (println banner)
+      (write-solution (:input options) (:output options))))
+  (flush))
